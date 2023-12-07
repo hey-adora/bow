729717ef96af5ec02ede88548bc253394ccac516
night <>
2023-12-07 13:06:17 +0200

    feature: writing text

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,27 @@ use std::{
 };
 use thiserror::Error;
 
+struct Editor<'a> {
+    line: &'a str,
+}
+
+impl<'a> Widget for Editor<'a> {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        // let mut temp = [0; 4];
+        // let x = self.line.encode_utf8(&mut temp);
+        // buf.set_string(0, 0, string, style)
+        // buf.get_mut(0, 0).set_string(self.line);
+
+        buf.set_string(0, 0, self.line, Style::new());
+    }
+}
+
+impl<'a> Editor<'a> {
+    pub fn new(c: &'a str) -> Self {
+        Editor { line: c }
+    }
+}
+
 fn main() {
     let mut terminal = setup_terminal().unwrap();
     run(&mut terminal).unwrap();
@@ -19,28 +40,43 @@ fn main() {
 
 fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), RunError> {
     // terminal.flush()?;
-    let mut switch = true;
+    // let mut switch = true;
+    // let mut line: &str = "";
+    let mut line: String = String::new();
     loop {
-        // terminal.draw(crate::render_app)?;
+        terminal.draw(|frame| {
+            frame.render_widget(Editor::new(&line), frame.size());
+        })?;
 
-        let b = terminal.current_buffer_mut();
-        let p = b.get_mut(0, 0);
-        p.set_symbol(match switch {
-            true => {
-                switch = false;
-                "x"
+        // if should_quit()? {
+        //     break;
+        // }
+
+        if let Event::Key(key) = event::read()? {
+            if let KeyCode::Esc = key.code {
+                break;
+            } else if let KeyCode::Backspace = key.code {
+                line.pop();
+            } else if let KeyCode::Char(key) = key.code {
+                line.push(key);
             }
-            false => {
-                switch = true;
-                "y"
-            }
-        });
-        terminal.flush()?;
-        terminal.hide_cursor()?;
-        terminal.swap_buffers();
-        if should_quit()? {
-            break;
         }
+
+        // let b = terminal.current_buffer_mut();
+        // let p = b.get_mut(0, 0);
+        // p.set_symbol(match switch {
+        //     true => {
+        //         switch = false;
+        //         "x"
+        //     }
+        //     false => {
+        //         switch = true;
+        //         "y"
+        //     }
+        // });
+        // terminal.flush()?;
+        // terminal.hide_cursor()?;
+        // terminal.swap_buffers();
     }
     Ok(())
 }
