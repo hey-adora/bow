14df83685c881184fed29b91e7bfa9216b256336
night <>
2023-12-07 14:26:32 +0200

    feature: app state

--- src/main.rs
+++ src/main.rs
@@ -7,110 +7,115 @@ use crossterm::{
 use ratatui::{prelude::*, widgets::*};
 use std::{
     io::{self, Stdout},
+    marker::PhantomData,
+    ops::Deref,
+    ops::DerefMut,
+    rc::Rc,
+    sync::{Arc, RwLock},
     time::Duration,
 };
 use thiserror::Error;
 
-struct Editor<'a> {
-    line: &'a str,
+struct Editor<'a, T> {
+    phantom: PhantomData<&'a T>,
 }
 
-impl<'a> Widget for Editor<'a> {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        // let mut temp = [0; 4];
-        // let x = self.line.encode_utf8(&mut temp);
-        // buf.set_string(0, 0, string, style)
-        // buf.get_mut(0, 0).set_string(self.line);
-
-        buf.set_string(0, 0, self.line, Style::new());
+impl<'a, T> StatefulWidget for Editor<'a, T> {
+    type State = AppState;
+    fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
+        buf.set_string(0, 0, &state.line, Style::new());
     }
 }
 
-impl<'a> Editor<'a> {
-    pub fn new(c: &'a str) -> Self {
-        Editor { line: c }
+impl<'a> Editor<'a, PhantomData<()>> {
+    pub fn new() -> Self {
+        Editor {
+            phantom: PhantomData,
+        }
+    }
+}
+
+struct AppState {
+    line: String,
+}
+
+impl AppState {
+    pub fn new() -> Self {
+        Self {
+            line: String::new(),
+        }
+    }
+}
+
+struct App {
+    terminal: Terminal<CrosstermBackend<Stdout>>,
+    state: AppState,
+}
+
+// impl Deref for App {
+//     type Target = Self;
+//     fn deref(&self) -> &Self::Target {
+//         &self
+//     }
+// }
+//
+// impl DerefMut for App {
+//     fn deref_mut(&mut self) -> &mut Self::Target {
+//         &mut self
+//     }
+// }
+
+impl App {
+    pub fn new() -> Self {
+        let terminal = App::setup_terminal().unwrap();
+        let state = AppState::new();
+        Self { terminal, state }
+    }
+
+    pub fn run(&mut self) -> Result<(), RunError> {
+        // let mut app = Arc::new(RwLock::new(self));
+        // let mut app = app.write().unwrap();
+        loop {
+            self.terminal.draw(|frame| {
+                // let mut app = app.clone().write().unwrap();
+                frame.render_stateful_widget(Editor::new(), frame.size(), &mut self.state);
+            })?;
+
+            if let Event::Key(key) = event::read()? {
+                if let KeyCode::Esc = key.code {
+                    break;
+                } else if let KeyCode::Backspace = key.code {
+                    self.state.line.pop();
+                } else if let KeyCode::Char(key) = key.code {
+                    self.state.line.push(key);
+                }
+            }
+        }
+        App::restore_terminal(&mut self.terminal).unwrap();
+        Ok(())
+    }
+
+    fn setup_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>, SetupTerminalError> {
+        let mut stdout = io::stdout();
+        enable_raw_mode()?;
+        execute!(stdout, EnterAlternateScreen)?;
+        let backend = Terminal::new(CrosstermBackend::new(stdout))?;
+        Ok(backend)
+    }
+
+    fn restore_terminal(
+        terminal: &mut Terminal<CrosstermBackend<Stdout>>,
+    ) -> Result<(), RestoreTerminalError> {
+        disable_raw_mode()?;
+        execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
+        terminal.show_cursor()?;
+        Ok(())
     }
 }
 
 fn main() {
-    let mut terminal = setup_terminal().unwrap();
-    run(&mut terminal).unwrap();
-    restore_terminal(&mut terminal).unwrap();
-}
-
-fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), RunError> {
-    // terminal.flush()?;
-    // let mut switch = true;
-    // let mut line: &str = "";
-    let mut line: String = String::new();
-    loop {
-        terminal.draw(|frame| {
-            frame.render_widget(Editor::new(&line), frame.size());
-        })?;
-
-        // if should_quit()? {
-        //     break;
-        // }
-
-        if let Event::Key(key) = event::read()? {
-            if let KeyCode::Esc = key.code {
-                break;
-            } else if let KeyCode::Backspace = key.code {
-                line.pop();
-            } else if let KeyCode::Char(key) = key.code {
-                line.push(key);
-            }
-        }
-
-        // let b = terminal.current_buffer_mut();
-        // let p = b.get_mut(0, 0);
-        // p.set_symbol(match switch {
-        //     true => {
-        //         switch = false;
-        //         "x"
-        //     }
-        //     false => {
-        //         switch = true;
-        //         "y"
-        //     }
-        // });
-        // terminal.flush()?;
-        // terminal.hide_cursor()?;
-        // terminal.swap_buffers();
-    }
-    Ok(())
-}
-
-fn render_app(frame: &mut Frame) {
-    let greeting = Paragraph::new("Hello World! (press 'q' to quit)");
-    frame.render_widget(greeting, frame.size());
-}
-
-fn should_quit() -> Result<bool, ShouldQuitError> {
-    if event::poll(Duration::from_millis(250))? {
-        if let Event::Key(key) = event::read()? {
-            return Ok(KeyCode::Char('q') == key.code);
-        }
-    }
-
-    Ok(false)
-}
-
-fn setup_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>, SetupTerminalError> {
-    let mut stdout = io::stdout();
-    enable_raw_mode()?;
-    execute!(stdout, EnterAlternateScreen)?;
-    let backend = Terminal::new(CrosstermBackend::new(stdout))?;
-    Ok(backend)
-}
-
-fn restore_terminal(
-    terminal: &mut Terminal<CrosstermBackend<Stdout>>,
-) -> Result<(), RestoreTerminalError> {
-    disable_raw_mode()?;
-    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
-    terminal.show_cursor()?;
-    Ok(())
+    let mut app = App::new();
+    app.run().unwrap();
 }
 
 #[derive(Error, Debug)]
